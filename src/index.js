94824b9b5dd2f0e0571e0f105b2ab7f46f0079ed
smanaenkov <smanaenkov@pmpractice.ru>
2018-04-28 11:53:51 +0300

    Рефакторинг 2

--- src/index.js
+++ src/index.js
@@ -77,7 +77,7 @@ function returnCounter(number = 0) {
    returnArgumentsArray(1, 2, 3) вернет [1, 2, 3]
  */
 function returnArgumentsArray() {
-    return [].slice.call(arguments);
+    return [...arguments];
 }
 
 /*
